cfb55b48d183e2e2014b8766cc72ad4a4f49ec53
Zack Piispanen <olink@shankshock.com>
2012-06-02 16:52:31 -0400

    Updated 1.12

--- C3Mod.cs
+++ C3Mod.cs
@@ -15,7 +15,7 @@ using TShockAPI.DB;
 
 namespace C3Mod
 {
-    [APIVersion(1, 11)]
+    [APIVersion(1, 12)]
     public class C3Mod : TerrariaPlugin
     {
         public static C3ConfigFile C3Config { get; set; }
